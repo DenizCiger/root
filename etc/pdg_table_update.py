590ca8b18f1cb30c1d2acbab4c3193abcd6cb544
Deniz Ciger <deniz.ciger@gmx.at>
2023-07-27 12:20:52 +0200

    Added some comments. Improved readability of code.

--- etc/pdg_table_update.py
+++ etc/pdg_table_update.py
@@ -1,38 +1,31 @@
 import urllib.request
 
-massURL = "https://pdg.lbl.gov/2023/mcdata/mass_width_2023.txt"
-tableURL = "https://raw.githubusercontent.com/root-project/root/master/etc/pdg_table.txt"
+MASS_URL = "https://pdg.lbl.gov/2023/mcdata/mass_width_2023.txt"
+TABLE_URL = "https://raw.githubusercontent.com/root-project/root/master/etc/pdg_table.txt"
+
+# Character positions for extracting data from "mass_width_2023.txt"
+MASS_WIDTH_ID_START = 0
+MASS_WIDTH_ID_END = 8
+MASS_WIDTH_MASS_START = 33
+MASS_WIDTH_MASS_END = 51
+MASS_WIDTH_WIDTH_START = 70
+MASS_WIDTH_WIDTH_END = 88
+MASS_WIDTH_NAME_WITH_CHARGE_START = 107
+MASS_WIDTH_NAME_WITH_CHARGE_END = 128
 
 #------------------------
 # Gets the text document
 #------------------------
 
-def getFileText(link):
-    with urllib.request.urlopen(link) as f:
-        text = f.read().decode("utf-8")
-        lines = text.splitlines()
-        return lines
-
-#------------------------------
-# Create updated pdg_table.txt
-#------------------------------
-
-def createTable():
-    outputFile = ""
-
-    for line in originalTable:
-        if (line.strip().startswith('#')):
-            lineToWrite = f"{line}\n"
-        else:
-            if line.split()[0].isdigit() and line.split()[1].isdigit():
-                lineToWrite = f"{line}\n"
-            else:
-                lineToWrite = fixedParticleData(line)
-
-        outputFile += lineToWrite
-
-    with open("etc/pdg_table.txt", 'w') as file:
-        file.write(outputFile)
+def getFileTextFromURL(link):
+    try:
+        with urllib.request.urlopen(link) as f:
+            text = f.read().decode("utf-8")
+            lines = text.splitlines()
+            return lines
+    except Exception as e:
+        print(f"Error while fetching data from URL: {e}")
+        return None
 
 #-------------------------------------
 # Gets the correct Data for particles
@@ -42,25 +35,27 @@ def fixedParticleData(infoLine):
     elements = infoLine.split()
     
     name = elements[1]
-    trueID = int(elements[2])
+    trueId = int(elements[2])
 
-    if (len(elements) < 6): # If it's an Antiparticle
-            return f"{elements[0]:>5} {name:<16} {trueID:>6} {elements[3]:>7} {elements[4]:>5}\n"
-    else: # If it's not an Antiparticle
+    if len(elements) < 6:  # If it's an Antiparticle
+        return f"{elements[0]:>5} {name:<16} {trueId:>6} {elements[3]:>7} {elements[4]:>5}\n"
+    else:  # If it's not an Antiparticle
         mass = float(elements[7])
         width = float(elements[8])
             
-        if (trueID in massWidthData):
-            if (massWidthData[trueID]['Mass']):
-                mass = float(massWidthData[trueID]['Mass'])
-            if (massWidthData[trueID]['Width']):
-                width = float(massWidthData[trueID]['Width'])
+        if trueId in massWidthData:
+            if massWidthData[trueId]['Mass']:
+                mass = float(massWidthData[trueId]['Mass'])
+            if massWidthData[trueId]['Width']:
+                width = float(massWidthData[trueId]['Width'])
                 
-        return f"{elements[0]:>5} {name:<16} {trueID:>6} {elements[3]:>2} {elements[4]:>3} {elements[5]:<10} {elements[6]:>2} {mass:<11e} {width:<11e} {elements[9]:>3} {elements[10]:>2} {elements[11]:>3} {elements[12]:>2} {elements[13]:>4} {elements[14]:>3}\n"
+        # Formatting the output line with the updated mass and width
+        return f"{elements[0]:>5} {name:<16} {trueId:>6} {elements[3]:>2} {elements[4]:>3} {elements[5]:<10} {elements[6]:>2} {mass:<11e} {width:<11e} {elements[9]:>3} {elements[10]:>2} {elements[11]:>3} {elements[12]:>2} {elements[13]:>4} {elements[14]:>3}\n"
 
 #---------------------------------
 # Save the data of the mass_width
 #---------------------------------
+
 def getMassWidthValues(lines):
     data = {}  # Dictionary to store the data
     for line in lines:
@@ -68,19 +63,19 @@ def getMassWidthValues(lines):
             continue  # Skip comments
 
         # Extracting relevant information based on the character column positions
-        particleId = line[0:8].strip()
+        particleId = line[MASS_WIDTH_ID_START:MASS_WIDTH_ID_END].strip()
 
-        if (line[33:51].strip()):
-            mass = line[33:51].strip()
+        if line[MASS_WIDTH_MASS_START:MASS_WIDTH_MASS_END].strip():
+            mass = line[MASS_WIDTH_MASS_START:MASS_WIDTH_MASS_END].strip()
         else:
-            mass = 0.0 # No value is given
+            mass = 0.0  # No value is given
         
-        if (line[70:88].strip()):
-            width = line[70:88].strip()
+        if line[MASS_WIDTH_WIDTH_START:MASS_WIDTH_WIDTH_END].strip():
+            width = line[MASS_WIDTH_WIDTH_START:MASS_WIDTH_WIDTH_END].strip()
         else:
-            width = 0.0 # No value is given
+            width = 0.0  # No value is given
 
-        nameWithCharge = line[107:128].strip()
+        nameWithCharge = line[MASS_WIDTH_NAME_WITH_CHARGE_START:MASS_WIDTH_NAME_WITH_CHARGE_END].strip()
         name = nameWithCharge.split()[0]  # Extract only the name, excluding the charge
 
         # Storing the data in the dictionary
@@ -91,14 +86,41 @@ def getMassWidthValues(lines):
 
     return data
 
+#------------------------------
+# Create updated pdg_table.txt
+#------------------------------
+
+def createTable():
+    if originalTable is None or massWidthData is None:
+        print("Data not available. Aborting table creation.")
+        return
+
+    outputLines = []
+
+    for line in originalTable:
+        if line.strip().startswith('#'):
+            outputLines.append(f"{line}\n")
+        else:
+            if line.split()[0].isdigit() and line.split()[1].isdigit():
+                outputLines.append(f"{line}\n")
+            else:
+                outputLines.append(fixedParticleData(line))
+
+    with open("etc/pdg_table.txt", 'w') as file:
+        file.writelines(outputLines)
 
 #------------------
 # Setup everything
 #------------------
 
-massDataLines = getFileText(massURL)
-originalTable = getFileText(tableURL)
+# Fetch mass_width_2023.txt and pdg_table.txt content from URLs
+massDataLines = getFileTextFromURL(MASS_URL)
+originalTable = getFileTextFromURL(TABLE_URL)
 
-massWidthData = getMassWidthValues(massDataLines)
+# Check if the data was fetched successfully before proceeding
+if massDataLines:
+    massWidthData = getMassWidthValues(massDataLines)
+else:
+    massWidthData = None
 
 createTable()
